ef78afa7d54e1964da081b969dbb176291884638
DevNathanC <nathancaetano599@gmail.com>
2025-02-14 16:39:47 -0300

    adicionando eventos

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './card.css';
 
-const Card = ({children, color}) => {
+const Card = ({children, color, showCardColor}) => {
   return (
-    <div className="card" style={{ backgroundColor: color }}>
+    <div 
+    className="card" 
+    style={{ backgroundColor: color }} 
+    onClick={() =>showCardColor(color)}>
       {children}
     </div>
   );
--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,6 +1,12 @@
+import React from "react";
 import Card from "../card/card";
 
 const cardsTitle = ['Title Card 1', 'Title Card 2', 'Title Card 3'];
+
+const showCardColor = (color) => {
+    console.log('Card clicado');
+    
+}
 
 const Cards = () => {
     return (
@@ -9,12 +15,18 @@ const Cards = () => {
 
             <div>
                 {cardsTitle.map((cardTitle, index) => (
-                        <Card key={index}>
-                            <h3>{cardTitle}</h3>
-                            <p>Texto do card 1</p>
-                        </Card>
-                    )
+                    <Card key={index} showCardColor={showCardColor}>
+                        <h3>{cardTitle}</h3>
+                        <p>Texto do card 1</p>
+                    </Card>
+                )
                 )}
+
+                <Card color='blue'>
+                    <h3>Card com fundo azul</h3>
+                    <p>Texto do card 1</p>
+                </Card>
+
             </div>
         </div>
     );
